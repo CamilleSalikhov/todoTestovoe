36b49c0cdd6931b559427499e5c92a020a0db714
your_username <dontlookbackinanger@yandex.ru>
2022-11-27 23:59:10 +0300

    test css

--- src/store/index.js
+++ src/store/index.js
@@ -20,7 +20,7 @@ const persistState = getLocalStorage();
 const store = createStore(rootReducer, persistState,
     compose(
         applyMiddleware(...middleware),
-         
+        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
     )
      
